3b9fc7f5de920b15943a43cce8bd55264b2bb29d
brennan-lee <113056160+brennan-lee@users.noreply.github.com>
2023-01-12 17:59:37 -0500

    js

--- js/script.js
+++ js/script.js
@@ -10,14 +10,23 @@
  * Check servie worker.
  */
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS2O-PWA-Test/sw.js", {
-    scope: "/ICS2O-PWA-Test/",
+  navigator.serviceWorker.register("/ICS2O-5-07/sw.js", {
+    scope: "/ICS2O-5-07/",
   })
 }
 
 /**
- * This function displays an alert.
+ * This function does multiplication.
  */
 function myButtonClicked() {
-  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
+  var numA = parseFloat(document.getElementById("firstInputedNumber").value)
+  var product = 0
+  var counter = 0
+
+  while (counter <= numA) {
+    product = product + counter
+    counter++
+  }
+
+  document.getElementById("product").innerHTML = "The product is: " + product
 }
